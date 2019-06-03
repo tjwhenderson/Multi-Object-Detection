2bc4427111f49fbea8fd146625dda5bddc039542
shbhas <shubhabhas@gmail.com>
2019-06-03 08:22:22 -0700

    Add files via upload
    deform net

---
+++ yolo_model_dcnn (2).py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+import torch.nn as nn
+import numpy as np
+from collections import OrderedDict
+import math
+import cv2
+import torchvision as tv
+from PIL import Image
+from scipy.ndimage.interpolation import map_coordinates as sp_map_coordinates
+from torch.autograd import Variable
+
+
+# In[3]:
+
+
+class dcnnyoloModel(nn.Module):
+    def __init__(self, config):
+        super(dcnnyoloModel, self).__init__()
+        self.config = config
+        self.backbone = darknet53(self.config["backbone_pretrained"])
+        _out_filters = self.backbone.layers_out_filters
+        final_out_filter0 = len(config["anchors"][0]) * (5 + config["classes"])
+        self.embedding0 = self._make_embedding([512, 1024], _out_filters[-1], final_out_filter0)
+        
+        final_out_filter1 = len(config["anchors"][1]) * (5 + config["classes"])
+        self.embedding1_cbl = self._make_cbl(512, 256, 1)
+        self.embedding1_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.embedding1 = self._make_embedding([256, 512], _out_filters[-2] + 256, final_out_filter1)
+
+        final_out_filter2 = len(config["anchors"][2]) * (5 + config["classes"])
+        self.embedding2_cbl = self._make_cbl(256, 128, 1)
+        self.embedding2_upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.embedding2 = self._make_embedding([128, 256], _out_filters[-3] + 128, final_out_filter2)
+        
+    def _make_cbl(self, _in, _out, ks):
+        #add deform layer (offesets to pass into conv)
+        pad = (ks - 1) // 2
+        return nn.Sequential(OrderedDict([
+            ("deform", ConvOffset2D(_in)),
+            ("conv", nn.Conv2d(_in, _out, kernel_size=ks, stride=1, padding=pad, bias=False)),
+            ("bn", nn.BatchNorm2d(_out)),
+            ("relu", nn.LeakyReLU(0.1)),
+        ]))
+
+    def _make_embedding(self, filters_list, in_filters, out_filter):
+        m = nn.ModuleList([
+            self._make_cbl(in_filters, filters_list[0], 1),
+            self._make_cbl(filters_list[0], filters_list[1], 3),
+            self._make_cbl(filters_list[1], filters_list[0], 1),
+            self._make_cbl(filters_list[0], filters_list[1], 3),
+            self._make_cbl(filters_list[1], filters_list[0], 1),
+            self._make_cbl(filters_list[0], filters_list[1], 3)])
+        m.add_module("conv_out", nn.Conv2d(filters_list[1], out_filter, kernel_size=1,
+                                           stride=1, padding=0, bias=True))
+        return m
+    
+    def forward(self, x):
+        def _branch(_embedding, _in):
+            for i, e in enumerate(_embedding):
+                _in = e(_in)
+                if i == 4:
+                    out_branch = _in
+            return _in, out_branch
+        
+        x2, x1, x0 = self.backbone(x)
+        #  yolo branch 0
+        out0, out0_branch = _branch(self.embedding0, x0)
+        #  yolo branch 1
+        x1_in = self.embedding1_cbl(out0_branch)
+        x1_in = self.embedding1_upsample(x1_in)
+        x1_in = torch.cat([x1_in, x1], 1)
+        out1, out1_branch = _branch(self.embedding1, x1_in)
+        #  yolo branch 2
+        x2_in = self.embedding2_cbl(out1_branch)
+        x2_in = self.embedding2_upsample(x2_in)
+        x2_in = torch.cat([x2_in, x2], 1)
+        out2, out2_branch = _branch(self.embedding2, x2_in)
+        return out0, out1, out2
+    
+    def load_darknet_weights(self, weights_path):
+        import numpy as np
+        fp = open(weights_path, "rb")
+        header = np.fromfile(fp, dtype=np.int32, count=5)
+        # Needed to write header when saving weights
+        weights = np.fromfile(fp, dtype=np.float32)
+        fp.close()
+
+        ptr = 0
+        all_dict = self.state_dict()
+        all_keys = self.state_dict().keys()
+        print (all_keys)
+        last_bn_weight = None
+        last_conv = None
+        for i, (k, v) in enumerate(all_dict.items()):
+            if 'bn' in k:
+                if 'weight' in k:
+                    last_bn_weight = v
+                elif 'bias' in k:
+                    num_b = v.numel()
+                    vv = torch.from_numpy(weights[ptr:ptr + num_b]).view_as(v)
+                    v.copy_(vv)
+                    ptr += num_b
+                    # weight
+                    v = last_bn_weight
+                    num_b = v.numel()
+                    vv = torch.from_numpy(weights[ptr:ptr + num_b]).view_as(v)
+                    v.copy_(vv)
+                    ptr += num_b
+                    last_bn_weight = None
+                elif 'running_mean' in k:
+                    num_b = v.numel()
+                    vv = torch.from_numpy(weights[ptr:ptr + num_b]).view_as(v)
+                    v.copy_(vv)
+                    ptr += num_b
+                elif 'running_var' in k:
+                    num_b = v.numel()
+                    vv = torch.from_numpy(weights[ptr:ptr + num_b]).view_as(v)
+                    v.copy_(vv)
+                    ptr += num_b
+                    # conv
+                    v = last_conv
+                    num_b = v.numel()
+                    vv = torch.from_numpy(weights[ptr:ptr + num_b]).view_as(v)
+                    v.copy_(vv)
+                    ptr += num_b
+                    last_conv = None
+                else:
+                    raise Exception("Error for bn")
+            elif 'conv' in k:
+                if 'weight' in k:
+                    last_conv = v
+                else:
+                    num_b = v.numel()
+                    vv = torch.from_numpy(weights[ptr:ptr + num_b]).view_as(v)
+                    v.copy_(vv)
+                    ptr += num_b
+                    # conv
+                    v = last_conv
+                    num_b = v.numel()
+                    vv = torch.from_numpy(weights[ptr:ptr + num_b]).view_as(v)
+                    v.copy_(vv)
+                    ptr += num_b
+                    last_conv = None
+
+
+# In[2]:
+
+
+class ConvOffset2D(nn.Conv2d):
+    def __init__(self, filters, init_normal_stddev=0.01, **kwargs):
+        self.filters = filters
+        self._grid_param = None
+        super(ConvOffset2D, self).__init__(self.filters, self.filters*2, 3, padding=1, bias=False, **kwargs)
+        self.weight.data.copy_(self._init_weights(self.weight, init_normal_stddev))
+
+    def forward(self, x):
+        x_shape = x.size()
+        offsets = super(ConvOffset2D, self).forward(x)
+
+        # offsets: (b*c, h, w, 2)
+        offsets = self._to_bc_h_w_2(offsets, x_shape)
+
+        # x: (b*c, h, w)
+        x = self._to_bc_h_w(x, x_shape)
+
+        # X_offset: (b*c, h, w)
+        x_offset = th_batch_map_offsets(x, offsets, grid=self._get_grid(self,x))
+
+        # x_offset: (b, h, w, c)
+        x_offset = self._to_b_c_h_w(x_offset, x_shape)
+
+        return x_offset
+
+    @staticmethod
+    def _get_grid(self, x):
+        batch_size, input_height, input_width = x.size(0), x.size(1), x.size(2)
+        dtype, cuda = x.data.type(), x.data.is_cuda
+        self._grid_param = (batch_size, input_height, input_width, dtype, cuda)
+        self._grid = th_generate_grid(batch_size, input_height, input_width, dtype, cuda)
+        return self._grid
+
+    @staticmethod
+    def _init_weights(weights, std):
+        fan_out = weights.size(0)
+        fan_in = weights.size(1) * weights.size(2) * weights.size(3)
+        w = np.random.normal(0.0, std, (fan_out, fan_in))
+        return torch.from_numpy(w.reshape(weights.size()))
+
+    @staticmethod
+    def _to_bc_h_w_2(x, x_shape):
+        """(b, 2c, h, w) -> (b*c, h, w, 2)"""
+        x = x.contiguous().view(-1, int(x_shape[2]), int(x_shape[3]), 2)
+        return x
+
+    @staticmethod
+    def _to_bc_h_w(x, x_shape):
+        """(b, c, h, w) -> (b*c, h, w)"""
+        x = x.contiguous().view(-1, int(x_shape[2]), int(x_shape[3]))
+        return x
+
+    @staticmethod
+    def _to_b_c_h_w(x, x_shape):
+        """(b*c, h, w) -> (b, c, h, w)"""
+        x = x.contiguous().view(-1, int(x_shape[1]), int(x_shape[2]), int(x_shape[3]))
+        return x
+
+
+# In[4]:
+
+
+class Darknet(nn.Module):
+    ''' backbone architecture'''
+    def __init__(self, layers):
+        super(Darknet, self).__init__()
+        self.inplanes = 32
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.relu1 = nn.LeakyReLU(0.1)
+        
+        self.layer1 = self.make_layer([32, 64], layers[0])
+        self.layer2 = self.make_layer([64, 128], layers[1])
+        self.layer3 = self.make_layer([128, 256], layers[2])
+        self.layer4 = self.make_layer([256, 512], layers[3])
+        self.layer5 = self.make_layer([512, 1024], layers[4])
+        self.layers_out_filters = [64, 128, 256, 512, 1024]
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+    
+    
+    def make_layer(self, planes, blocks):
+        layers = []
+        #  downsample
+        layers.append(("ds_conv", nn.Conv2d(self.inplanes, planes[1], kernel_size=3,
+                                stride=2, padding=1, bias=False)))
+        layers.append(("ds_bn", nn.BatchNorm2d(planes[1])))
+        layers.append(("ds_relu", nn.LeakyReLU(0.1)))
+        #  blocks
+        self.inplanes = planes[1]
+        for i in range(0, blocks):
+            layers.append(("residual_{}".format(i), BasicBlock(self.inplanes, planes)))
+        return nn.Sequential(OrderedDict(layers))
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        out3 = self.layer3(x)
+        out4 = self.layer4(out3)
+        out5 = self.layer5(out4)
+
+        return out3, out4, out5
+
+
+# In[5]:
+
+
+def darknet53(pretrained, **kwargs):
+        model = Darknet([1, 2, 8, 8, 4])
+        if pretrained:
+            if isinstance(pretrained, str):
+                model.load_state_dict(torch.load(pretrained))
+            else:
+                raise Exception("darknet request a pretrained path. got [{}]".format(pretrained))
+        return model
+
+
+# In[6]:
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, inplanes, planes):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes[0], kernel_size=1,
+                               stride=1, padding=0, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes[0])
+        self.relu1 = nn.LeakyReLU(0.1)
+        self.conv2 = nn.Conv2d(planes[0], planes[1], kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes[1])
+        self.relu2 = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
+
+        out += residual
+        return out
+
+
+# In[ ]:
+
+
+def th_batch_map_offsets(input, offsets, grid=None, order=1):
+    """Batch map offsets into input
+    Parameters
+    ---------
+    input : torch.Tensor. shape = (b, s, s)
+    offsets: torch.Tensor. shape = (b, s, s, 2)
+    Returns
+    -------
+    torch.Tensor. shape = (b, s, s)
+    """
+    batch_size = input.size(0)
+    input_height = input.size(1)
+    input_width = input.size(2)
+
+    offsets = offsets.view(batch_size, -1, 2)
+    if grid is None:
+        grid = th_generate_grid(batch_size, input_height, input_width, offsets.data.type(), offsets.data.is_cuda)
+
+    coords = offsets + grid
+    
+    mapped_vals = th_batch_map_coordinates(input, coords)
+    return mapped_vals
+
+
+# In[9]:
+
+
+def th_generate_grid(batch_size, input_height, input_width, dtype, cuda):
+    grid = np.meshgrid(
+        range(input_height), range(input_width), indexing='ij'
+    )
+    grid = np.stack(grid, axis=-1)
+    grid = grid.reshape(-1, 2)
+
+    grid = np_repeat_2d(grid, batch_size)
+    grid = torch.from_numpy(grid).type(dtype)
+    if cuda:
+        grid = grid.cuda()
+    return Variable(grid, requires_grad=False)
+
+def np_repeat_2d(a, repeats):
+    """Tensorflow version of np.repeat for 2D"""
+
+    assert len(a.shape) == 2
+    a = np.expand_dims(a, 0)
+    a = np.tile(a, [repeats, 1, 1])
+    return a
+
+
+# In[10]:
+
+
+def th_batch_map_coordinates(input, coords, order=1):
+    """Batch version of th_map_coordinates
+    Only supports 2D feature maps
+    Parameters
+    ----------
+    input : tf.Tensor. shape = (b, s, s)
+    coords : tf.Tensor. shape = (b, n_points, 2)
+    Returns
+    -------
+    tf.Tensor. shape = (b, s, s)
+    """
+
+    batch_size = input.size(0)
+    input_height = input.size(1)
+    input_width = input.size(2)
+
+    n_coords = coords.size(1)
+
+    # coords = torch.clamp(coords, 0, input_size - 1)
+
+    coords = torch.cat((torch.clamp(coords.narrow(2, 0, 1), 0, input_height - 1), torch.clamp(coords.narrow(2, 1, 1), 0, input_width - 1)), 2)
+
+    assert (coords.size(1) == n_coords)
+
+    coords_lt = coords.floor().long()
+    coords_rb = coords.ceil().long()
+    coords_lb = torch.stack([coords_lt[..., 0], coords_rb[..., 1]], 2)
+    coords_rt = torch.stack([coords_rb[..., 0], coords_lt[..., 1]], 2)
+    idx = th_repeat(torch.arange(0, batch_size), n_coords).long()
+    idx = Variable(idx, requires_grad=False)
+    if input.is_cuda:
+        idx = idx.cuda()
+
+    def _get_vals_by_coords(input, coords):
+        indices = torch.stack([
+            idx, th_flatten(coords[..., 0]), th_flatten(coords[..., 1])
+        ], 1)
+        inds = indices[:, 0]*input.size(1)*input.size(2)+ indices[:, 1]*input.size(2) + indices[:, 2]
+        vals = th_flatten(input).index_select(0, inds)
+        vals = vals.view(batch_size, n_coords)
+        return vals
+
+    vals_lt = _get_vals_by_coords(input, coords_lt.detach())
+    vals_rb = _get_vals_by_coords(input, coords_rb.detach())
+    vals_lb = _get_vals_by_coords(input, coords_lb.detach())
+    vals_rt = _get_vals_by_coords(input, coords_rt.detach())
+
+    coords_offset_lt = coords - coords_lt.type(coords.data.type())
+    vals_t = coords_offset_lt[..., 0]*(vals_rt - vals_lt) + vals_lt
+    vals_b = coords_offset_lt[..., 0]*(vals_rb - vals_lb) + vals_lb
+    mapped_vals = coords_offset_lt[..., 1]* (vals_b - vals_t) + vals_t
+    return mapped_vals
+
+
+# In[12]:
+
+
+def th_repeat(a, repeats, axis=0):
+    """Torch version of np.repeat for 1D"""
+    assert len(a.size()) == 1
+    return th_flatten(torch.transpose(a.repeat(repeats, 1), 0, 1))
+
+def th_flatten(a):
+    """Flatten tensor"""
+    return a.contiguous().view(a.nelement())
+
