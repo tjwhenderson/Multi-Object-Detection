40814223ddc781cb3e30ca6406bcc15bb87c3bd2
silver-deguzz <j5deguzm@j5deguzm-30197.pods.j5deguzm.svc.cluster.local>
2019-06-03 12:07:17 -0700

    created demo notebook

--- detect.py
+++ detect.py
@@ -1,114 +1,85 @@
-from utils import compute_AP, NMS
-from yolo_model import yoloModel
+# from utils import compute_AP, NMS
+# from yolo_model import yoloModel
 
-from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import Compose, Resize, RandomHorizontalFlip, RandomVerticalFlip, ToTensor, Normalize
-
-import PASCAL_Dataloader
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-import time
-import PIL
+from matplotlib.ticker import NullLocator
+# matplotlib.use('Agg')
+from PIL import Image
+
 
 CLASS_ID = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", \
                 "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", \
                 "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
-def detect(test_loader, test_data, is_training=False):
-    ## TODO: Load YOLO model
 
-    print('Performing object detection!')
 
-    use_cuda = torch.cuda.is_available()
-    # Setup GPU optimization if CUDA is supported
-    if use_cuda:
-        computing_device = torch.device("cuda")
-        print("CUDA is supported")
-    else: # Otherwise, train on the CPU
-        computing_device = torch.device("cpu")
-
-    imgs = []
-    img_detections = []  # Stores detections for each image index
-    img_labels = []
-
-    for batch_i, samples in enumerate(test_loader):
-        images, labels = samples["image"], samples["label"]
-
-        images = images.to(computing_device)
-        labels = labels.to(computing_device)
-        with torch.no_grad():
-            detections = net(images)
-            detections = NMS(detections)
-
-        # save for plotting
-        imgs.extend(images)
-        img_detections.extend(detections)
-        img_labels.extend(labels)
-    return imgs, img_detections, img_labels
-
-def plot_bbox(img, detections, label=None):
-    """ img : the original pre-transformed image
-        detections : (x1, y1, x2, y2, obj_conf, cls_score, cls_pred)
-        label : ground truth label
-    """
-
+def plot_detections(imgpath, targets, predictions, batch, sample, output_dir, is_demo=False):
+    cmap = plt.get_cmap('tab20b')
+    colors = [cmap(i) for i in np.linspace(0, 1, 100)]
+    bbox_colors = random.sample(colors, 40)
+    
     plt.figure()
-    fig, ax = plt.subplots(1)
-    ax.imshow(img)
+    fig, ax = plt.subplots(1, 2, figsize=(8,4))
+    img = Image.open(imgpath).convert('RGB')
+    ax[0].imshow(img)
+    ax[1].imshow(img)
+    ax[0].set_title('Groundtruth Bounding Boxes')
+    ax[1].set_title('Predicted Bounding Boxes')
+    ax[0].axis('off')
+    ax[1].axis('off')
 
-    ORI_H, ORI_W = img.shape[:2]
-    H, W = 416, 416
-
-    # Draw bounding boxes and labels of detections
-    if detections is not None:
-        unique_labels = detections[:, -1]  # get all cls_pred
-        n_cls_preds = len(unique_labels)  # get unique num
-        bbox_colors = random.sample(colors, n_cls_preds)  # color for each
-
-        # These preds are sorted by classes, then by conf (in each class)
-        cur_class = -1
-
-        for pred_i, (x1, y1, x2, y2, obj_conf, cls_score, cls_pred) in enumerate(detections):
-            cls_pred = cls_pred.item()
-            cls_id = CLASS_ID[int(cls_pred)]
-
-            # Rescale coordinates to original dimensions
-            x = x1 * (ORI_W / W)
-            y = y1 * (ORI_H / H)
-            w = abs(x2 - x1) * (ORI_W / W)
-            h = abs(y2 - y1) * (ORI_H / H)
-
-            color = bbox_colors[int(cls_pred)]
-            if cur_class != cls_pred:
-                # This means the pred is the top one of that class
-                cls_name = "*" + cls_id
-                zorder = 20
-                cur_class = cls_pred
-            else:
-                cls_name = cls_id
-                zorder = 4
-
-             # Create a Rectangle patch
-            bbox = patches.Rectangle((x, y), w, h, linewidth=2,
-                                     edgecolor=color, facecolor='none',
-                                     zorder=zorder)
-
-            # Add the bbox and label to the plot
-            ax.add_patch(bbox)
-            plt.text(x, y, s=cls_id, color='white',
-                             verticalalignment='top',
-                             bbox={'color': color, 'pad': 0},
-                             fontsize=8, zorder=zorder)
-
-    else:
-        print('No objects detected!')
-
-    plt.axis('off')
-    # plt.gca().xaxis.set_major_locator(NullLocator())
-    # plt.gca().yaxis.set_major_locator(NullLocator())
+    # -------------------------- #
+    # Plotting groundtruth boxes #
+    # -------------------------- #
+    for jj in range(len(targets)):
+        cls, x, y, w, h = targets[jj,0], targets[jj,1], targets[jj,2], targets[jj,3], targets[jj,4]
+        color = bbox_colors[cls]
+        # Create a Rectangle patches
+        bbox = patches.Rectangle((x, y), w, h, linewidth=2, \
+                                 edgecolor=color, facecolor='none')
+        # Add the bbox to the plot
+        ax[0].add_patch(bbox)
+        # Add label
+        ax[0].text(x, y, s=CLASS_ID[cls], color='white', \
+                 verticalalignment='top',bbox={'color': color, 'pad': 0})
+        
+    # ------------------------ #
+    # Plotting predicted boxes #
+    # ------------------------ #
+    for jj in range(len(predictions)):
+        cls, x, y, w, h = predictions[jj,0], predictions[jj,1], \
+                          predictions[jj,2], predictions[jj,3], predictions[jj,4]
+        color = bbox_colors[cls+20]
+        # Create a Rectangle patches
+        bbox = patches.Rectangle((x, y), w, h, linewidth=2, \
+                                 edgecolor=color, facecolor='none')
+        # Add the bbox to the plot
+        ax[1].add_patch(bbox)
+        # Add label
+        ax[1].text(x, y, s=CLASS_ID[cls], color='black', \
+                 verticalalignment='top',bbox={'color': color, 'pad': 0})
 
     # Save generated image with detections
-    # pth = os.path.join(output_folder_path, '{}.png'.format(img_i))
-    # plt.savefig(pth, bbox_inches='tight', pad_inches=0.0)
-    # plt.close()
+    plt.axis('off')
+    plt.gca().xaxis.set_major_locator(NullLocator())
+    plt.gca().yaxis.set_major_locator(NullLocator())
+    
+    if not is_demo: # only saves the detected images if it's not for the demo
+        output_file = output_dir + '/{}_{}.jpg'.format(batch, sample)
+        plt.savefig(output_file, bbox_inches='tight', pad_inches=0.0)
+        plt.close()
+#     else:
+#         plt.close()
+
+    
+    
+
+            
+
+            
+            
+
+
